137018115803555b501af35e8cf2389aa52722e1
NimishVadhwa <vadhwanimish20@gmail.com>
2022-05-02 13:28:57 +0530

    'new'

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const db = require('./src/database');
+let port = process.env.PORT || 7000;
+var cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+    next();
+})
+
+//error handling
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ status: false, message: err.message });
+});
+
+db
+    .sync()
+    .then((result) => {
+        app.listen(port);
+        console.log('Db connected===', port);
+    }).catch((err) => {
+        console.log('db not connected');
+        console.log(err);
+    });
+
